d7400509776b6c21024d557c18c21b62e07eaa9b
Chinthala naveen <Chinthalanaveen504@gmail.com>
2025-03-15 21:49:33 +0530

    test now

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:3000', ],
+  origin: ['http://localhost:3000', 'https://naveenchinthala.netlify.app'],
   methods: ['GET', 'POST', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Accept'],
   credentials: true
