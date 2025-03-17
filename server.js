b88ed7fd2da8a083a94b2119f9d4728e026255f0
Chinthala naveen <Chinthalanaveen504@gmail.com>
2025-03-17 11:31:07 +0530

    findOne added and mongoose connect update

--- server.js
+++ server.js
@@ -80,7 +80,7 @@ app.post("/Form", async (req, res) => {
 // Display all form submissions
 app.get("/Display", async (req, res) => {
   try {
-    const Display = await Form.findOne(); // Fetch all documents in the "Form" collection
+    const Display = await Form.findOne(); 
     res.json(Display); // Send the data as JSON response
   } catch (err) {
     console.error("Error fetching data:", err);
